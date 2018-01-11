847506111f08eafae54585fe61ece3f7f8f74995
masterzion <master.zion@gmail.com>
2018-01-11 11:49:11 +0100

    Update init_cluster.sh
    added bind_ip_all and hostname

--- init_cluster.sh
+++ init_cluster.sh
@@ -40,45 +40,50 @@ sudo chmod -R 770  /mongodb/
 
 #--configsvrMode=sccc
 # config servers
-sudo -u mongod mongod --configsvr --dbpath /mongodb/cfg0 --port 26050 --replSet conf --fork --logpath /mongodb/log.cfg0 --logappend
-sudo -u mongod mongod --configsvr --dbpath /mongodb/cfg1 --port 26051 --replSet conf --fork --logpath /mongodb/log.cfg1 --logappend
-sudo -u mongod mongod --configsvr --dbpath /mongodb/cfg2 --port 26052 --replSet conf --fork --logpath /mongodb/log.cfg2 --logappend
+sudo -u mongod mongod --configsvr --dbpath /mongodb/cfg0 --port 26050 --replSet conf --fork --logpath /mongodb/log.cfg0 --logappend --bind_ip_all
+sudo -u mongod mongod --configsvr --dbpath /mongodb/cfg1 --port 26051 --replSet conf --fork --logpath /mongodb/log.cfg1 --logappend --bind_ip_all
+sudo -u mongod mongod --configsvr --dbpath /mongodb/cfg2 --port 26052 --replSet conf --fork --logpath /mongodb/log.cfg2 --logappend --bind_ip_all
 
 # "shard servers" (mongod data servers)
 # note: don't use small files nor such a small oplogSize in production; these are here as we are running many on one 
-sudo -u mongod mongod --shardsvr --replSet a --dbpath /mongodb/a0 --logpath /mongodb/log.a0 --port 27000 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet a --dbpath /mongodb/a1 --logpath /mongodb/log.a1 --port 27001 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet a --dbpath /mongodb/a2 --logpath /mongodb/log.a2 --port 27002 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet b --dbpath /mongodb/b0 --logpath /mongodb/log.b0 --port 27100 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet b --dbpath /mongodb/b1 --logpath /mongodb/log.b1 --port 27101 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet b --dbpath /mongodb/b2 --logpath /mongodb/log.b2 --port 27102 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet c --dbpath /mongodb/c0 --logpath /mongodb/log.c0 --port 27200 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet c --dbpath /mongodb/c1 --logpath /mongodb/log.c1 --port 27201 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet c --dbpath /mongodb/c2 --logpath /mongodb/log.c2 --port 27202 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet d --dbpath /mongodb/d0 --logpath /mongodb/log.d0 --port 27300 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet d --dbpath /mongodb/d1 --logpath /mongodb/log.d1 --port 27301 --fork --logappend --smallfiles --oplogSize 50
-sudo -u mongod mongod --shardsvr --replSet d --dbpath /mongodb/d2 --logpath /mongodb/log.d2 --port 27302 --fork --logappend --smallfiles --oplogSize 50
+sudo -u mongod mongod --shardsvr --replSet a --dbpath /mongodb/a0 --logpath /mongodb/log.a0 --port 27000 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet a --dbpath /mongodb/a1 --logpath /mongodb/log.a1 --port 27001 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet a --dbpath /mongodb/a2 --logpath /mongodb/log.a2 --port 27002 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet b --dbpath /mongodb/b0 --logpath /mongodb/log.b0 --port 27100 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet b --dbpath /mongodb/b1 --logpath /mongodb/log.b1 --port 27101 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet b --dbpath /mongodb/b2 --logpath /mongodb/log.b2 --port 27102 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet c --dbpath /mongodb/c0 --logpath /mongodb/log.c0 --port 27200 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet c --dbpath /mongodb/c1 --logpath /mongodb/log.c1 --port 27201 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet c --dbpath /mongodb/c2 --logpath /mongodb/log.c2 --port 27202 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet d --dbpath /mongodb/d0 --logpath /mongodb/log.d0 --port 27300 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet d --dbpath /mongodb/d1 --logpath /mongodb/log.d1 --port 27301 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
+sudo -u mongod mongod --shardsvr --replSet d --dbpath /mongodb/d2 --logpath /mongodb/log.d2 --port 27302 --fork --logappend --smallfiles --oplogSize 50 --bind_ip_all
 
 
-sudo -u mongod mongo --port 26050 --eval '
+sudo -u mongod mongo --port 26050 --eval "
 rs.initiate(
   {
-    _id: "conf",
+    _id: 'conf',
     configsvr: true,
     members: [
-      { _id : 0, host : "localhost:26050" },
-      { _id : 1, host : "localhost:26051" },
-      { _id : 2, host : "localhost:26052" }
+      { _id : 0, host : '$HOSTNAME:26050' },
+      { _id : 1, host : '$HOSTNAME:26051' },
+      { _id : 2, host : '$HOSTNAME:26052' }
     ]
   }
 )
-'
+"
+
+
+
 
 #mongos porcesses
-sudo -u mongod mongos --configdb "conf/localhost:26050,localhost:26051,localhost:26052" --fork --logappend --logpath /mongodb/log.mongos0 
-sudo -u mongod mongos --configdb "conf/localhost:26050,localhost:26051,localhost:26052" --fork --logappend --logpath /mongodb/log.mongos0 --port 26061
-sudo -u mongod mongos --configdb "conf/localhost:26050,localhost:26051,localhost:26052" --fork --logappend --logpath /mongodb/log.mongos0 --port 26062
-sudo -u mongod mongos --configdb "conf/localhost:26050,localhost:26051,localhost:26052" --fork --logappend --logpath /mongodb/log.mongos0 --port 26063
+sudo -u mongod mongos --configdb "conf/$HOSTNAME:26050,$HOSTNAME:26051,$HOSTNAME:26052" --fork --logappend --logpath /mongodb/log.mongos0  --bind_ip_all
+sudo -u mongod mongos --configdb "conf/$HOSTNAME:26050,$HOSTNAME:26051,$HOSTNAME:26052" --fork --logappend --logpath /mongodb/log.mongos0 --port 26061 --bind_ip_all
+sudo -u mongod mongos --configdb "conf/$HOSTNAME:26050,$HOSTNAME:26051,$HOSTNAME:26052" --fork --logappend --logpath /mongodb/log.mongos0 --port 26062 --bind_ip_all
+sudo -u mongod mongos --configdb "conf/$HOSTNAME:26050,$HOSTNAME:26051,$HOSTNAME:26052" --fork --logappend --logpath /mongodb/log.mongos0 --port 26063 --bind_ip_all
+
+
 
 sudo -u mongod mongo  --port 26050  --eval 'rs.status()'
 
